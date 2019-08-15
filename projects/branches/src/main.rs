5a86cf8242db9c3bb4effb7811561477a3ecb4f9
Hilgard <hilgardvr@gmail.com>
2019-08-15 05:30:42 +0200

    initial commit

---
+++ projects/branches/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+	let number = 8;
+
+	if number < 5 {
+    	println!("Hello, world!");
+	} else {
+		println!("condition was false");
+	}
+}
---
+++ projects/functions/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+
+	another_function(42);
+}
+
+
+fn another_function(x: i32) {
+	println!("Another function {}", x);
+}
